8e896836385521973c5b0d4475c0817abb361e68
Himanshu Kumar <himanshu.kumar133@gmail.com>
2015-11-07 02:26:45 +0530

    LRU Added

---
+++ LRU.py
@@ -0,0 +1,23 @@
+#Least Recently Used
+def lru(l):
+    frame=[-1]*3
+    co=[0]*3
+    count=-1
+    for i in range(len(l)):
+        if l[i] not in frame:
+            mi=min(co)
+            for j in range(len(co)):
+                if co[j]==mi:
+                    pos=j
+                    break
+            frame[pos]=l[i]
+            co[pos]=i+1
+        else:
+            for j in range(len(frame)):
+                if frame[j]==l[i]:
+                    pos=j
+                    break
+            co[pos]=i+1
+        print l[i], frame
+
+lru([2,3,2,1,5,2,4,5,3,2,5,2])
